79a1691b90f4de89dc745fb5c740fdcbb3d7b898
Anastasia <122746649+disturbiagirl@users.noreply.github.com>
2023-12-16 23:19:14 +0100

    completing from component

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,6 @@
-import NextAuth from "next-auth/next";
+import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+
 import User from "@models/user";
 import { connectToDb } from "@utils/database";
 
@@ -10,39 +11,36 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {
-    const sessionUser = await User.findOne({
-      email: session.user.email,
-    });
+  callbacks: {
+    async session({ session }) {
+      // store the user id from MongoDB to session
+      const sessionUser = await User.findOne({ email: session.user.email });
+      session.user.id = sessionUser._id.toString();
 
-    session.used.id = sessionUser._id.toString();
-    return session;
-  },
+      return session;
+    },
+    async signIn({ account, profile, user, credentials }) {
+      try {
+        await connectToDb();
 
-  // signIn function creates a new user if one does not exist yet
-  async signIn({ profile }) {
-    try {
-      await connectToDb();
+        // check if user already exists
+        const userExists = await User.findOne({ email: profile.email });
 
-      // check if a user already exists
-      const userExists = await User.findOne({
-        email: profile.email,
-      });
+        // if not, create a new document and save user in MongoDB
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
+        }
 
-      // if not create a new user
-      if (!userExists) {
-        await User.create({
-          email: profile.email,
-          username: profile.name.replace(" ", "").toLowerCase(),
-          image: profile.picture,
-        });
+        return true;
+      } catch (error) {
+        console.log("Error checking if user exists: ", error.message);
+        return false;
       }
-
-      return true;
-    } catch (error) {
-      console.log(error);
-      return false;
-    }
+    },
   },
 });
 
